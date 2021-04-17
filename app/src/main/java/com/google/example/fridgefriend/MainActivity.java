5ebe982e8d00d80098190c697cc45b0c32cf22bb
jessi <jli0801@bths.edu>
2021-04-17 12:40:37 -0400

    added console to firebase; next: work on auth

--- app/src/main/java/com/google/example/fridgefriend/MainActivity.java
+++ app/src/main/java/com/google/example/fridgefriend/MainActivity.java
@@ -7,6 +7,7 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,18 +15,43 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "";
-    TextView textLog, logT;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "CustomAuthActivity";
+    private String mCustomToken;
+    private static final int RC_SIGN_IN = 123;
+    private GoogleSignInClient mGoogleSignInClient;
     Button login;
 
     @Override
@@ -33,116 +59,149 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //setContentView(R.layout.home_page);
-        TextView logT = (TextView) findViewById(R.id.textLog);
-        logT.setVisibility(View.INVISIBLE);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        Button login = (Button) findViewById(R.id.loginButton);
-        findViewById(R.id.loginButton).setOnClickListener(accListner);
-        //buttons
-        findViewById(R.id.homePage).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,HomePage.class);
 
-                startActivity(intent);
-            }
-        });
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //non null
+        mAuth = FirebaseAuth.getInstance();
+        //update UI
 
     }
 
-    private View.OnClickListener accListner = new View.OnClickListener() {
-        public void onClick(View v) {
-            AccountManager accMan = AccountManager.get(getApplicationContext());
-            Intent googlePicker = AccountManager.newChooseAccountIntent(null, null,
-                    new String[] {"com.google"}, true, null, null, null, null);
-            startActivityForResult(googlePicker, 1);
-            login.setVisibility(Button.INVISIBLE);
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
 
-        }
-    };
 
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1 && resultCode == RESULT_OK){
-            Bundle options = new Bundle();
-            AccountManager accountManager = AccountManager.get(this);
-            Account[] accounts = accountManager.getAccounts();
-            for(Account a : accounts){
-                Log.d(TAG, "type--" + a.type + "---name---" + a.name);
-                accountManager.invalidateAuthToken(a.type, null);
-                accountManager.getAuthToken(
-                        a,
-                        "Manager your tasks",
-                        options,
-                        this,
-                        new OnTokenAcquired(),
-                        new Handler(new OnError()));
 
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+            }
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
             }
         }
     }
+    // [END auth_fui_result]
 
-    private class Handler extends android.os.Handler{
-        public Handler(OnError onError){
-
-        }
-
-    }
-
-
-
-    private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
-
-
-        public class networkThread implements Runnable
-        {
-            AccountManagerFuture<Bundle> result;
-            public networkThread(AccountManagerFuture<Bundle> result)
-            {
-                this.result = result;
-            }
-
-            public void run(){
-                try {
-                    Intent launch = (Intent) result.getResult().get((AccountManager.KEY_INTENT));
-                    if(launch != null){
-                        startActivityForResult(launch, 0);
-                        return;
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
                     }
-                    Bundle bundle = null;
-                    bundle = result.getResult();
-
-                    String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
-
-                    String apiKey = "AIzaSyBZyXazssSe3L-GrYJ9f29lfkEKiSd3ZFY";
-                    String clientID = "716402394253-elcpt5ku3srbavkbpgaho4oae2esuv7l.apps.googleusercontent.com";
-                    String clientSecret = "";
-                    URL url = new URL("https://www.googleapis.com/tasks/v1/users/@me/lists?key=" + apiKey);
-                    URLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.addRequestProperty("client_id", clientID);
-                    conn.addRequestProperty("client_secret", clientSecret);
-                    conn.setRequestProperty("Authorization", "OAuth " + token);
-                    logT.setVisibility(View.VISIBLE);
-
-
-                } catch (AuthenticatorException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (OperationCanceledException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        @Override
-        public void run(AccountManagerFuture<Bundle> result) {
-            Thread t = new Thread(new networkThread(result));
-            t.start();
-        }
+                });
     }
 
-    private class OnError {
+    public void getUserProfile() {
+        // [START get_user_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+
+            String uid = user.getUid();
+        }
+        // [END get_user_profile]
     }
+
+    // [START signin]
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signin]
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+    /*
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+
+    public void delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_delete]
+    }
+
+    public void themeAndLogo() {
+        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+
+        // [START auth_fui_theme_logo]
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.logo)      // Set logo drawable
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_theme_logo]
+    }*/
+
 
 }
+
+
