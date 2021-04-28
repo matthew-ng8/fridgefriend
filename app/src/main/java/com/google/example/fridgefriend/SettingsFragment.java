d6ff776f87488a4f9c69cae8db796e084c7515ee
jessi <jli0801@bths.edu>
2021-04-28 00:26:55 -0400

    adding onclick to SettingsFragment

--- app/src/main/java/com/google/example/fridgefriend/SettingsFragment.java
+++ app/src/main/java/com/google/example/fridgefriend/SettingsFragment.java
@@ -1,22 +1,27 @@
 package com.google.example.fridgefriend;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        /*
+
         Preference signout = (Preference) findPreference("signout");
         signout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-               MainActivity.signOut();
+                FirebaseAuth.getInstance().signOut();
+                return true;
             }
-        });*/
+        });
     }
 
 
