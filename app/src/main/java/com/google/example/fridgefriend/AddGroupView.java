e673fa26e34d15a373d930c0b26a75318d209c71
jessi <jli0801@bths.edu>
2021-05-05 22:36:17 -0400

    worked on add group

--- app/src/main/java/com/google/example/fridgefriend/AddGroupView.java
+++ app/src/main/java/com/google/example/fridgefriend/AddGroupView.java
@@ -1,5 +1,6 @@
 package com.google.example.fridgefriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,27 +19,33 @@ public class AddGroupView extends AppCompatActivity {
         setContentView(R.layout.addgroupview);
         Button enterG = (Button) findViewById(R.id.enterGroup);
         Button enterNew = (Button) findViewById(R.id.newGroup);
-        EditText fieldG = (EditText) findViewById(R.id.existingGroup);
-        EditText fieldNew = (EditText) findViewById(R.id.addGroup);
+        final EditText fieldG = (EditText) findViewById(R.id.existingGroup);
+        final EditText fieldNew = (EditText) findViewById(R.id.addGroup);
 
         enterG.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent enterIntent = new Intent();
 
+                enterIntent.putExtra("oldG", fieldG.getText().toString());
+                setResult(RESULT_OK, enterIntent);
+                finish();
             }
         });
 
         enterNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent newIntent = new Intent();
 
+                newIntent.putExtra("NewG", fieldNew.getText().toString());
+                setResult(RESULT_OK, newIntent);
+                finish();
             }
         });
 
 
-
-
-
-
     }
+
+
 }
